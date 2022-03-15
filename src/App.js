8755ff5c81d8486b671b0154acd9677b9eba9284
Adrian <adriancubillos@gmail.com>
2022-03-14 23:48:46 -0500

    Added Navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.componentt';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the Shop</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
