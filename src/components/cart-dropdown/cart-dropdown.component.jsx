24025f4a673cfa2914b0ad47c0ae97a20561865f
Adrian <adriancubillos@gmail.com>
2022-03-16 18:20:23 -0500

    cart contex extensibility addtocart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ReactComponent as EmptyShoppingCart } from '../../assets/empty-cart.svg';
+import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
-
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items' />
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <div className='empty-cart'>
+            <EmptyShoppingCart className='empty-cart-image' />
+            <span className='empty-message'>Your cart is empty!</span>
+          </div>
+        )}
+      </div>
       <Button>Go TO CHECKOUT</Button>
     </div>
   );
