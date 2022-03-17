004598d5dbd4504da18b8ecedadf3bfe3e5f92c8
Adrian <adriancubillos@gmail.com>
2022-03-17 12:56:00 -0500

    Sub-Routing and class clash fix

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import ProductCard from '../product-card/product-card.component';
 import './category-preview.styles.scss';
 
 export const CategoryPreview = ({ title, products }) => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate(`${title}`);
+  };
+
   return (
     <div className='category-preview-container'>
       <h2>
-        <span className='title'>{title.toUpperCase()}</span>
+        <span className='title' onClick={navigateHandler}>
+          {title.toUpperCase()}
+        </span>
       </h2>
       <div className='preview'>
         {products
